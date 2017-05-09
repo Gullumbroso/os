b54a1200739c1b6e942ab79778ab6ca63491d28a
gullumbroso <gullumbroso@gmail.com>
2017-05-09 17:59:35 +0300

    hi

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -2,15 +2,15 @@
 // Created by Gilad Lumbroso on 08/05/2017.
 //
 
-#include "stdlib.h"
-#include "stdio.h"
-#include "string"
-#include "vector"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
 #include "MapReduceFramework.h"
 #include <pthread.h>
 #include <iostream>
 #include <semaphore.h>
-#include <stdlib.h>
+#include <map>
 
 using namespace std;
 typedef pair<k2Base*, v2Base*> SHUFFLE_ITEM;
@@ -30,11 +30,17 @@ pthread_t shuffleThread;
 
 
 vector<pthread_t *> execMapThreads;
+vector<SHUFFLE_VEC> execMapContainers;
 vector<void*> threadsArgs;
 sem_t semaphore;
 
 int k1v1Index;
+
+map<pthread_t, int> pthreadToContainer;
+
+// MUTEXES
 pthread_mutex_t k1v1Mutex;
+pthread_mutex_t pthreadToContainer_mutex;
 
 // METHODS
 void prepareMappingPhase(MapReduceBase &mapReduceBase, int multiThreadLevel);
@@ -60,7 +66,7 @@ void init()
     k1v1Index = 0;
     k1v1Mutex = PTHREAD_MUTEX_INITIALIZER;
 
-    // creates the shuffle thread
+    // Create the shuffle thread
     sem_init(&semaphore, 0, 1);
     int res = pthread_create(&shuffleThread, NULL, shuffle, &k2v2Container);
     if (res < 0)
@@ -68,6 +74,13 @@ void init()
         exitWithError("pthread_create");
     }
 
+    // Create and lock the ptheradToContainer_mutex
+    pthreadToContainer_mutex = PTHREAD_MUTEX_INITIALIZER;
+    int res = pthread_mutex_lock(&pthreadToContainer_mutex);
+    if (res < 0)
+    {
+        exitWithError("pthread_mutex_lock");
+    }
 }
 
 
@@ -77,25 +90,45 @@ RunMapReduceFramework(MapReduceBase &mapReduce, IN_ITEMS_VEC &itemsVec, int mult
 {
     init();
     prepareMappingPhase(mapReduce, multiThreadLevel);
+
+    int res = pthread_mutex_unlock(&pthreadToContainer_mutex);
+    if (res < 0)
+    {
+        exitWithError("pthread_mutex_lock");
+    }
+
     k1v1Container = itemsVec;
 
     return OUT_ITEMS_VEC();
 }
 
+
 void prepareMappingPhase(MapReduceBase &mapReduceBase, int multiThreadLevel)
 {
     // Create <multiThreadLevel> number of threads, if needed
     int numOfThreadsCount = 0;
+    int containerIdx = 0;
     while (numOfThreadsCount < k1v1Container.size() && (int) execMapThreads.size < multiThreadLevel)
     {
         pthread_t thread;
+
         int res = pthread_create(&thread, NULL, mapExecFunc, &mapReduceBase);
         if (res < 0)
         {
             exitWithError("pthread_create");
         }
+
         execMapThreads.push_back(&thread);
+
+        // Create the corresponding container
+        SHUFFLE_VEC container;
+        execMapContainers.push_back(container);
+
         numOfThreadsCount += CHUNK_SIZE;
+
+        // Creating the pthreadToContainer map
+        pthreadToContainer.insert(pair<pthread_t, int>(thread, containerIdx));
+        containerIdx++;
     }
 }
 
@@ -104,13 +137,37 @@ void prepareMappingPhase(MapReduceBase &mapReduceBase, int multiThreadLevel)
  */
 void *mapExecFunc(void *mrb)
 {
+    // Waiting for the ptheradToContainer map to initialize
+    int res = pthread_mutex_lock(&pthreadToContainer_mutex);
+    if (res < 0)
+    {
+        exitWithError("pthread_mutex_lock");
+    }
+    res = pthread_mutex_unlock(&pthreadToContainer_mutex);
+    if (res < 0)
+    {
+        exitWithError("pthread_mutex_unlock");
+    }
+
     MapReduceBase *mapReduceBase = (MapReduceBase *) mrb;
     unsigned long containerSize = k1v1Container.size();
 
     while (k1v1Index < containerSize) {
 
-        // Take a batch from the container
+        pthread_mutex_lock(&k1v1Mutex);
+
+        // Check if the k1v1Index value hasn't been modified since the entrance to the loop
+        if (k1v1Index >= containerSize)
+        {
+            break;
+        }
+
         int chunkSize = (int) min(containerSize - k1v1Index, CHUNK_SIZE);
+        k1v1Index += chunkSize;
+
+        pthread_mutex_unlock(&k1v1Mutex);
+
+        // Take a batch from the container
         vector<IN_ITEM>::const_iterator first = k1v1Container.begin() + k1v1Index;
         vector<IN_ITEM>::const_iterator last = k1v1Container.begin() + chunkSize;
         vector<IN_ITEM> chunk(first, last);
@@ -126,9 +183,30 @@ void *mapExecFunc(void *mrb)
     pthread_exit(NULL);
 }
 
+/**
+ * @brief The library funciton the emits a <k2,v2> pair into the container of the current thread.
+ * @param k2Base A pointer to the k2base.
+ * @param v2Base A pointer to the v2base.
+ */
+void Emit2(k2Base *k2Base, v2Base *v2Base)
+{
+    pthread_t threadID = pthread_self();
+    int idx = pthreadToContainer.at(threadID);
+    SHUFFLE_VEC container = execMapContainers[idx];
+    container.push_back(pair(k2Base, v2Base));
+
+
+}
+
 
 void *shuffle(void *args){
     int res = sem_wait(&semaphore);
 
     pthread_exit(NULL);
 }
+
+void Emit3(k3Base *k3Base, v3Base *v3Base) {
+
+}
+
+// TODO: delete and destroy all the resources that we used.
